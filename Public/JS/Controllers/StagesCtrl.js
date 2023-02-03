2c436044961f0b0b6271a55cfac3817c251fad95
Logfer <molnar.viktor999@gmail.com>
2023-02-03 09:18:02 +0100

    news page complete, stages begin

--- Public/JS/Controllers/StagesCtrl.js
+++ Public/JS/Controllers/StagesCtrl.js
@@ -1,3 +1,22 @@
 app.controller('StagesCtrl',function($scope){
+    $scope.StagesEras =[
+        {
+            Title: "Cell Stage",
+            Firstrow: "Evolution routes",
+            Secondrow: "Each evolution route have 4 phase"
+        },
+        {
+            Title: "Creature Stage",
+            Firstrow: "Basic management",
+            Secondrow: "Skills to unlock",
+            Thirdrow: "Small herd of specie"
+        },
+        {
+            Title: "Tribal Stage",
+            Firstrow: "Small settlement",
+            Secondrow: "Job management",
+            Thirdrow: "Researching"
+        }
+    ]
 
 });
