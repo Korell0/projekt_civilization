3a91731fb16886aff589c99a23c68284401af33b
Logfer <molnar.viktor999@gmail.com>
2023-02-22 11:34:46 +0100

    Rework the cell stage

--- Game/JS/Controllers/CivCtrl.js
+++ Game/JS/Controllers/CivCtrl.js
@@ -1,33 +1,43 @@
 app.controller('CivCtrl',function($scope,$rootScope, DB, $interval){
     $scope.buttons = $rootScope.buttons
 
+    $interval(function(){
+        $rootScope.resources[0].quantity = $rootScope.resources[0].quantity + $rootScope.RNAI;
+    }, 1000)
 
     $scope.Click = function(id,idx){
-        if($rootScope.buttons[idx].Name === "RNA"){
-            if($rootScope.resources[0].quantity < $rootScope.resources[0].storage){
+        if($rootScope.buttons[idx].Evolution === 0 && $rootScope.buttons[idx].storage === 0 && $rootScope.buttons[idx].producer === 0){
+            if($rootScope.buttons[idx].Name === "RNA"){
+                if($rootScope.resources[0].quantity =< $rootScope.resources[0].storage){
+
+                }
                 $rootScope.resources[0].quantity++;
             }
-        }
-        if($rootScope.buttons[idx].Name === "DNA"){
-            if($rootScope.resources[1].quantity < $rootScope.resources[1].storage){
+            if($rootScope.buttons[idx].Name === "DNA"){
                 if($rootScope.resources[0].quantity >= 2){
                     $rootScope.resources[0].quantity = $rootScope.resources[0].quantity - 2;
                     $rootScope.resources[1].quantity++;
                 }
             }
         }
-        if($rootScope.buttons[idx].Name === "Membrane"){
-            if($rootScope.resources[1].quantity >= parseInt($rootScope.buttons[idx].DNA)){
-                $rootScope.resources[0].storage += 10;
-                $rootScope.resources[1].quantity = $rootScope.resources[1].quantity - parseInt($rootScope.buttons[idx].DNA);
-                $rootScope.buttons[idx].DNA = parseInt($rootScope.buttons[idx].DNA) + 10;
+        else{
+            if($rootScope.buttons[idx].storage === 1){
+                if($rootScope.buttons[idx].DNA > 0 || $rootScope.buttons[idx].RNA > 0){
+                    if($rootScope.resources[1].quantity >= parseInt($rootScope.buttons[idx].DNA) && $rootScope.resources[0].quantity >= parseInt($rootScope.buttons[idx].RNA)){
+                        $rootScope.resources[1].quantity = $rootScope.resources[1].quantity - parseInt($rootScope.buttons[idx].DNA)
+                        $rootScope.resources[0].quantity = $rootScope.resources[0].quantity - parseInt($rootScope.buttons[idx].RNA)
+                        $rootScope.buttons[idx].DNA = parseInt($rootScope.buttons[idx].DNA) + parseInt($rootScope.buttons[idx].DNAplus);
+                        $rootScope.buttons[idx].RNA = parseInt($rootScope.buttons[idx].RNA) + parseInt($rootScope.buttons[idx].RNAplus);
+                        if($rootScope.buttons[idx].storageRNAplus > 0){
+                            $rootScope.resources[0].storage += $rootScope.buttons[idx].storageRNAplus;
+                        }
+                        if($rootScope.buttons[idx].storageDNAplus > 0){
+                            $rootScope.resources[1].storage += $rootScope.buttons[idx].storageDNAplus;
+                        }
+                    }
+                }
             }
         }
-    }
-    $scope.Soul = function(){
-        var interval = $interval(function(){
-            console.log("helloooo")
-        },1000)
     }
 });
 
